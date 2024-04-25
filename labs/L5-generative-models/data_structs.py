fa8ebcd7d888776c5a489ccc046968089f94346c
Tomasz Danel <15916779+mokosaur@users.noreply.github.com>
2024-04-25 08:16:54 +0200

    Remove file name collisions

--- labs/L5-generative-models/data_structs.py
+++ labs/L5-generative-models/data_structs.py
@@ -8,7 +8,7 @@ import time
 import torch
 from torch.utils.data import Dataset
 
-from utils import Variable
+from utils_reinvent import Variable
 
 class Vocabulary(object):
     """A class for handling encoding/decoding from SMILES to an array of indices"""
